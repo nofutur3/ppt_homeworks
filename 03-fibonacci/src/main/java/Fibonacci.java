0fbb6333296b6e823f4905789e6bacc124b07a99
Jakub Vyvazil <jakub@vyvazil.cz>
2018-12-09 19:45:42 +0100

    added generated javadoc for fibonacci

--- 03-fibonacci/src/main/java/Fibonacci.java
+++ 03-fibonacci/src/main/java/Fibonacci.java
@@ -1,9 +1,18 @@
 import java.security.InvalidParameterException;
 
+/**
+ * Trida pro vypocet fibonacciho posloupnosti
+ */
 public class Fibonacci {
 
     private int[] cache = new int[1000];
 
+    /**
+     *  Vypocet fibonacciho posloupnosti pomoci rekurze s vyuzitim cache
+     *
+     * @param   position    kolikate cislo fibanocciho posloupnosti ma metoda vratit
+     * @return              vraci vysledne cislo fibonacciho posloupnosti
+     */
     public int getResultForPosition(int position) {
         if (position < 0 || position > 1000) {
             throw new InvalidParameterException("Position has to be number between 0 and 1000");
@@ -11,6 +20,13 @@ public class Fibonacci {
 
         return this.calculateFibonacci(position);
     }
+
+    /**
+     *  Vypocet fibonacciho posloupnosti pomoci cyklu
+     *
+     * @param   position    kolikate cislo fibanocciho posloupnosti ma metoda vratit
+     * @return              vraci vysledne cislo fibonacciho posloupnosti
+     */
     public int getResultForPositionCycle(int position) {
         if (position < 0 || position > 1000) {
             throw new InvalidParameterException("Position has to be number between 0 and 1000");
@@ -19,7 +35,13 @@ public class Fibonacci {
         return this.calculateFibonacciWithForCycle(position);
     }
 
-    //  0, 1, 1, 2, 3, 5, 8, 13, 21
+    /**
+     * Implementace fibonacciho posloupnosti - algoritmu
+     * za pomocí for rekurze a s vyuzitim cache
+     *
+     * @param   position    kolikate cislo fibanocciho posloupnosti ma metoda vratit
+     * @return              vraci vysledne cislo fibonacciho posloupnosti
+     */
     private int calculateFibonacci(int position) {
         if (position == 0) return 0;
         if (position == 1) return 1;
@@ -32,6 +54,13 @@ public class Fibonacci {
         return result;
     }
 
+    /**
+     * Implementace fibonacciho posloupnosti - algoritmu
+     * za pomocí for cyklu
+     *
+     * @param   position    kolikate cislo fibanocciho posloupnosti ma metoda vratit
+     * @return              vraci vysledne cislo fibonacciho posloupnosti
+     */
     private int calculateFibonacciWithForCycle(int position) {
         if (position == 0) return 0;
 
